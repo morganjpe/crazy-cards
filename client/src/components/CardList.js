30a85075ab1979910236e30b99a797b6f71a9d09
Evans <evansm54@GBSFYVL001M7922.local>
2020-12-08 07:58:30 +0000

    add preloader

--- client/src/components/CardList.js
+++ client/src/components/CardList.js
@@ -1,25 +1,38 @@
 import PropTypes from "prop-types";
 import tw, { styled } from "twin.macro";
+import { BeatLoader } from "react-spinners";
 
 import Card from "./Card";
 
 const CardList = ({ cards }) => {
   return (
-    <>
-      <CardList.Container>
-        {cards.map((card) => (
-          <Card key={`card-${card.id}`} card={card} />
-        ))}
-      </CardList.Container>
-    </>
+    <CardList.Container>
+      {cards.length ? (
+        <ol>
+          {cards.map((card) => (
+            <Card key={`card-${card.id}`} card={card} />
+          ))}
+        </ol>
+      ) : (
+        <div className="loader" style={{ margin: "0 auto" }}>
+          <BeatLoader color={tw`bg-blue-400`} />
+        </div>
+      )}
+    </CardList.Container>
   );
 };
 
-CardList.Container = styled.ol`
-  ${tw`w-full lg:w-3/4 bg-white flex border-l border-gray-200`}
-  flex-flow: column;
+CardList.Container = styled.section`
+  ${tw`w-full lg:w-3/4 bg-white flex border-l border-gray-200 relative`}
   height: 1131px;
   padding: 15px;
+
+  .loader {
+    ${tw`flex justify-center absolute`}
+    top: 50%;
+    left: 50%;
+    transform: translateY(-50%);
+  }
 `;
 
 CardList.propTypes = {
